e910dcd574f984457f5ae7e2297314edab26634f
Robin Tail <robin_tail@me.com>
2020-09-03 11:01:48 +0200

    Style changes for widgets page.

--- src/components/widgets-page/index.jsx
+++ src/components/widgets-page/index.jsx
@@ -7,6 +7,11 @@ import Header from 'semantic-ui-react/dist/commonjs/elements/Header';
 import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
 import SegmentGroup from 'semantic-ui-react/dist/commonjs/elements/Segment/SegmentGroup';
 import {WidgetsListConnected} from '../widgets-list';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Container)`
+  padding-top: 40px;
+`;
 
 class WidgetsPage extends React.Component {
   onAddWidgetClick() {
@@ -15,8 +20,8 @@ class WidgetsPage extends React.Component {
 
   render() {
     return (
-      <Container text>
-        <Header size='large'>Widgets Page</Header>
+      <StyledContainer text>
+        <Header size='large' textAlign='center'>Widgets Page</Header>
         <SegmentGroup>
           <Segment textAlign='right'>
             <Button
@@ -30,7 +35,7 @@ class WidgetsPage extends React.Component {
             <WidgetsListConnected />
           </Segment>
         </SegmentGroup>
-      </Container>
+      </StyledContainer>
     );
   }
 }
